cc80d14c6b1934b2620ed61a8f27e1ea0acdea31
Jose Morrone <josemmorrone@gmail.com>
2023-06-14 16:21:58 -0400

    Se agrega la otra funcion para enviarle el correo al cliente

--- src/data.js
+++ src/data.js
@@ -7,26 +7,26 @@ export const topCarteras = [
     {
         modelo: "ALANYS3",
         nombre: "ALANYS",
-        img: Alanys3,
+        img: "https://res.cloudinary.com/drwck4b6d/image/upload/v1685985105/ALANYS-3_bcnoha.jpg",
         precio: 45, 
         material: "Cuero"
     },
     {
         modelo: "SILVIA1",
         nombre: "SILVIA",
-        img: Silvia1,
+        img: "https://res.cloudinary.com/drwck4b6d/image/upload/v1685985110/SILVIA-1_piyiwt.jpg",
         precio: 60
     },
     {
         modelo: "ALANYS2",
         nombre: "ALANYS",
-        img: Alanys2,
+        img: 'https://res.cloudinary.com/drwck4b6d/image/upload/v1685985104/ALANYS-2_sofhhb.jpg',
         precio: 45
     },
     {
         modelo: "SILVIA2",
         nombre: "SILVIA",
-        img: Silvia2,
+        img: "https://res.cloudinary.com/drwck4b6d/image/upload/v1685985108/SILVIA-2_eptcmg.jpg",
         precio: 60
     },
 ]
