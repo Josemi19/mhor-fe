7923a5adf54dbe373c6acc59a81afa92495c216b
Jose Morrone <josemmorrone@gmail.com>
2023-06-26 15:49:23 -0400

    [UPGRADE] Se hace responsive la vista de single product

--- src/js/views/singleProduct.jsx
+++ src/js/views/singleProduct.jsx
@@ -20,21 +20,23 @@ export const SingleProduct = () => {
                     <div className="container mt-3 container-single" style={{ "height": "85vh" }}>
                         <div className='border border-dark bg-light container-foto'>
                             <img className='w-100 foto-single' src={cartera.img} alt="..." />
-                            <div className='d-flex justify-content-between p-3 caption-single'>
-                                <div>
-                                    <h3>{cartera.nombre}</h3>
-                                    <h6>{`Material: ${cartera.material}`}</h6>
+                            <div className='d-flex flex-column justify-content-between'>
+                                <div className='d-flex justify-content-between p-3 caption-single'>
+                                    <div>
+                                        <h3>{cartera.nombre}</h3>
+                                        <h6>{`Material: ${cartera.material}`}</h6>
+                                    </div>
+                                    <h6>{`$${cartera.precio} USD`}</h6>
                                 </div>
-                                <h6>{`$${cartera.precio} USD`}</h6>
-                            </div>
 
-                            <div className='d-flex justify-content-end mb-2 p-3 button-single'>
-                                {existe == undefined
-                                    ?
-                                    <button className="btn btn-dark" onClick={() => actions.addToCart(cartera)}>Agregar al carrito</button>
-                                    :
-                                    <button className="btn btn-outline-danger" onClick={() => actions.addToCart(cartera)}>Eliminar del carrito</button>
-                                }
+                                <div className='d-flex justify-content-end mb-2 p-3 button-single'>
+                                    {existe == undefined
+                                        ?
+                                        <button className="btn btn-dark" onClick={() => actions.addToCart(cartera)}>Agregar al carrito</button>
+                                        :
+                                        <button className="btn btn-outline-danger" onClick={() => actions.addToCart(cartera)}>Eliminar del carrito</button>
+                                    }
+                                </div>
                             </div>
                         </div>
                         <ItemCar />
