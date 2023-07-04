9d21b8e95372344c6bccd05b980e391706cc972c
Jose Morrone <josemmorrone@gmail.com>
2023-07-04 16:20:04 -0400

    agrega la funcion getptrendas al appContext

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -22,6 +22,7 @@ const injectContext = PassedComponent => {
 
 		useEffect(() => {
 			state.actions.getCarteras()
+			state.actions.getPrendas()
 		}, []);
 
 		// The initial value for the context is not null anymore, but the current state of this component,
