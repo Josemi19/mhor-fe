183b542fe36c704b23365343659a073c9fc608cf
Jose Morrone <josemmorrone@gmail.com>
2023-07-06 14:20:46 -0400

    Cambio de diseno

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,19 +2,20 @@ import React from "react";
 import { Carousel } from "../component/Carousel.jsx";
 import { Cards } from "../component/Cards.jsx";
 import { ItemCar } from "../component/itemCar.jsx";
-import foto1 from "../../img/SILVIA-3.jpg"
+import foto1 from "../../img/foto1.jpg"
 
 
 export const Home = () => (
 	<div className="text-center" style={{"minHeight": "85vh"}}>
 		<Carousel />
 		<div className="container">
-			<Cards categoria={"carteras"}/>
+			<h1 className="my-3">PRODUCTOS</h1>
 			<div className="w-100 position-relative">
 				<img src={foto1} className="w-100 d-flex justify-content-end"></img>
 				<div className="fondo-home"></div>
 				<p className="texto-home">Queremos reafirmar tú estilo único e irrepetible</p>
 			</div>
+			<Cards categoria={"carteras"}/>
 			<Cards categoria={"prendas"}/>
 		</div>
 		<ItemCar />
