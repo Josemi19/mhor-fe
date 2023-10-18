108169178e0ce3e39f7e8b4c76a5802827b27e8e
Jose Morrone <josemmorrone@gmail.com>
2023-10-18 17:10:31 -0400

    cambios

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -17,10 +17,10 @@ export const Home = () => {
 			</Helmet>
 			<Carousel />
 			<section className="container">
-				<h1 className="mt-3 mb-1">LA MARCA</h1>
-				<p>Integra diseño, fabricación y comercialización de: ropa, carteras y accesorios. Todos los atuendos buscan que tu estilo se reafirme como único e irrepetible.</p>
-				<p>La idea es que cada vez que uses nuestras piezas te sientas identificad@, segur@, cómod@ y confiad@; ya que resaltarás tu estilo con prendas de  altísima calidad con sello venezolano.</p>
-				<p>Las tendencias evolucionan o simplemente pasan, pero el estilo perdura por siempre.</p>
+				<h1 className="mt-3 mb-1"><b>LA MARCA</b></h1>
+				<p className="fs-5">Integra diseño, fabricación y comercialización de: ropa, carteras y accesorios. Todos los atuendos buscan que tu estilo se reafirme como único e irrepetible.</p>
+				<p className="fs-5">La idea es que cada vez que uses nuestras piezas te sientas identificad@, segur@, cómod@ y confiad@; ya que resaltarás tu estilo con prendas de  altísima calidad con sello venezolano.</p>
+				<p className="fs-5">Las tendencias evolucionan o simplemente pasan, pero el estilo perdura por siempre.</p>
 				<div className="w-100 position-relative">
 					<img src={foto2} className="w-100 d-flex justify-content-end rounded"></img>
 					<div className="fondo-home rounded"></div>
